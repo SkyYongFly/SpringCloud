0355b31f3a170cbc793190eaba70c48d0e0c1179
SkyYongFly <yongzhu2014@outlook.com>
2018-10-08 15:57:48 +0800

    配置服务器和客户端纳入服务管理

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -3,9 +3,14 @@ package com.example.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @EnableDiscoveryClient 将配置客户端注册到服务注册中心
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class ConfigClientApplication {
--- config-server-git/src/main/java/com/example/configservergit/ConfigServerGitApplication.java
+++ config-server-git/src/main/java/com/example/configservergit/ConfigServerGitApplication.java
@@ -2,16 +2,21 @@ package com.example.configservergit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
- * 通过 @EnableConfigServer 注解声明本应用为Spring Cloud 配置服务器
+ * <p>
+ * 	通过 @EnableConfigServer 注解声明本应用为Spring Cloud 配置服务器<br>
+ * 	同时通过@EnableDiscoveryClient将应用注册到服务注册中心，方便后续构建配置服务器集群，实现高可用
+ * </p>
  *
  * @author  zhuyong
  * @date    2018/10/8 13:56
  * @version V1.0.0
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerGitApplication {
 
